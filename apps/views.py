7e26057d0f0a51ccce06b75c5c4c1064ddb83fcc
TSIDIANY Raharison Muriel <106076190+ITDevSuccess@users.noreply.github.com>
2024-04-11 19:20:52 +0300

    Rectification de views

--- apps/views.py
+++ apps/views.py
@@ -5,8 +5,18 @@ from apps.form import SearchForm, MachineForm
 
 # Create your views here.
 def index(request):
+    companies = []
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            companies = form.cleaned_data.get('company')
+        # print(companies, request.POST)
+
+    else:
+        form = SearchForm()
     context = {
-        'form': SearchForm(),
-        'form_add_machine': MachineForm()
+        'form': form,
+        'form_add_machine': MachineForm(),
+        'companies': companies
     }
     return render(request, 'apps/index.html', context)
