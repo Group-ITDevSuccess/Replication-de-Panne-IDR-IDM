2ac580f5af23044f721819e397c41779064fda5c
TSIDIANY Raharison Muriel <106076190+ITDevSuccess@users.noreply.github.com>
2024-04-22 08:50:58 +0300

    Fin de mise en place de developpement de suivie panne

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     CACHES = {
         "default": {
